4feeb1b82e8be47493a20405b92142f62f1263a6
lokky55 <lokky55@mail.ru>
2022-05-19 10:55:20 +0300

    Initial commit

---
+++ src/test/java/ru/netology/javaqa/IncomeServiceTest.java
@@ -0,0 +1,34 @@
+package ru.netology.javaqa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class IncomeServiceTest {
+
+    @Test
+    public void ShouldNoIncome() {
+        IncomeService service = new IncomeService();
+        int age = 2;
+        int actual = service.calcIncome(age);
+        int expected = 0;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldGetSalary() {
+        IncomeService service = new IncomeService();
+        int age = 50;
+        int actual = service.calcIncome(age);
+        int expected = 50_000;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldGetPension() {
+        IncomeService service = new IncomeService();
+        int age = 90;
+        int actual = service.calcIncome(age);
+        int expected = 20_000;
+        Assertions.assertEquals(expected, actual);
+    }
+}
